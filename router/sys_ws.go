3f0d26b47ed46d59c6b53a53da93c50956a739d4
1358826554 <1358826554@qq.com>
2021-06-21 17:59:02 +0800

    websocket服务更新，

---
+++ router/sys_ws.go
@@ -0,0 +1,13 @@
+package router
+import (
+"github.com/gin-gonic/gin"
+v1 "go-service/api/v1"
+)
+
+// InitWsRouter 用户路由集合
+func InitWsRouter(Router *gin.RouterGroup) {
+	UserRouter := Router.Group("ws")
+	{
+		UserRouter.GET("/v1ws", v1.WsPing)
+	}
+}
